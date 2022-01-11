cd8c7396a8332e440de299432921b4451f9b9eee
gahye0530 <40849083+gahye0530@users.noreply.github.com>
2022-01-11 17:13:36 +0900

    test1

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from resources.favorite import FavoriteListResource, FavoriteResource
 from resources.login import UserLoginResource
 from resources.logout import UserLogoutResource, jwt_blacklist
 from resources.register import UserRegisterResource
-from resources.movie import MovieInfoResource, MovieRealtimeRecommandResource, MovieRecommandResource, MovieSearchResource
+# from resources.movie import MovieInfoResource, MovieRealtimeRecommandResource, MovieRecommandResource, MovieSearchResource
 from resources.review import MovieReviewResource
 from resources.userinfo import UserInfoResource
 
@@ -29,12 +29,12 @@ api.add_resource(UserRegisterResource,'/api/v1/user/register')
 api.add_resource(UserLoginResource,'/api/v1/user/login')
 api.add_resource(UserInfoResource,'/api/v1/user/me')
 api.add_resource(UserLogoutResource, '/api/v1/user/logout')
-api.add_resource(MovieInfoResource, '/api/v1/movie')
-api.add_resource(MovieSearchResource, '/api/v1/movie/search')
+# api.add_resource(MovieInfoResource, '/api/v1/movie')
+# api.add_resource(MovieSearchResource, '/api/v1/movie/search')
 api.add_resource(MovieReviewResource, '/api/v1/review')
 api.add_resource(FavoriteResource, '/api/v1/favorit/<int:movie_id>')
 api.add_resource(FavoriteListResource, '/api/v1/favorit')
-# api.add_resource(MovieRecommandResource, '/api/v1/movie/recommendation')
-api.add_resource(MovieRealtimeRecommandResource, '/api/v1/movie/recommendation') # 실시간 추천
+# # api.add_resource(MovieRecommandResource, '/api/v1/movie/recommendation')
+# api.add_resource(MovieRealtimeRecommandResource, '/api/v1/movie/recommendation') # 실시간 추천
 
 if __name__ == '__main__' : app.run()
